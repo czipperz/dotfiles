5edcea11108f43ac51745df25c27232ab4ba8e8b
Czipperz <czipperz@gmail.com>
2015-03-26 16:39:19 -0500

    Removed magnifier setting

--- rc.lua
+++ rc.lua
@@ -66,7 +66,6 @@ local layouts =
     awful.layout.suit.spiral.dwindle,
     awful.layout.suit.max,
     awful.layout.suit.max.fullscreen,
-    awful.layout.suit.magnifier,
 	awful.layout.suit.floating
 }
 -- }}}
