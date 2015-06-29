0790bddfb9cac435a444f856d808e444461c5dd1
Czipperz <czipperz@gmail.com>
2015-06-28 22:38:40 -0500

    Needs to be symbolic links

--- installs/dotfiles-setup.sh
+++ installs/dotfiles-setup.sh
@@ -11,10 +11,10 @@ gcl czipperz/emacs.d $HOME/.emacs.d
 (cd chx; ./install)
 rm -R linux-tools
 [ -f .gitconfig ] && mv .gitconfig .gitconfig.bkup
-ln $HOME/dotfiles/git/gitconfig $HOME/.gitconfig
+ln -s $HOME/dotfiles/git/gitconfig $HOME/.gitconfig
 rm $HOME/.config/gr $HOME/.config/gcl
-ln $HOME/dotfiles/git/gcl       $HOME/.config/gcl
-ln $HOME/dotfiles/git/gcl       $HOME/.config/gr
+ln -s $HOME/dotfiles/git/gcl       $HOME/.config/gcl
+ln -s $HOME/dotfiles/git/gcl       $HOME/.config/gr
 
 #Setup awesome
 mkdir -p .config/awesome/themes
@@ -24,13 +24,13 @@ if [ ! -d .config/awesome/themes/transBlack ]; then
 	rm -R awesomeThemes
 fi
 
-ln $HOME/dotfiles/rc.lua                             $HOME/.config/awesome
-ln $HOME/dotfiles/zsh/czipperz.zsh-theme             $HOME/.oh-my-zsh/themes
+ln -s $HOME/dotfiles/rc.lua                      $HOME/.config/awesome
+ln -s $HOME/dotfiles/zsh/czipperz.zsh-theme      $HOME/.oh-my-zsh/themes
 [ -f .xinitrc ] && mv .xinitrc .xinitrc.bkup
-ln $HOME/dotfiles/xinitrc                            $HOME/.xinitrc
-ln $HOME/dotfiles/Xresources                         $HOME/.Xresources
-ln $HOME/dotfiles/zsh/zshrc                          $HOME/.zshrc
-ln $HOME/dotfiles/zsh/zlogin                         $HOME/.zlogin
+ln -s $HOME/dotfiles/xinitrc                     $HOME/.xinitrc
+ln -s $HOME/dotfiles/Xresources                  $HOME/.Xresources
+ln -s $HOME/dotfiles/zsh/zshrc                   $HOME/.zshrc
+ln -s $HOME/dotfiles/zsh/zlogin                  $HOME/.zlogin
 
 (cd b; ./install)
 rm -R b
