f8357bac0f5813ab4f1848c48dd9c1d9cfba7acf
Czipperz <czipperz@gmail.com>
2015-07-22 13:14:02 -0700

    Tabs into spaces

--- installs/dotfiles-setup.sh
+++ installs/dotfiles-setup.sh
@@ -20,9 +20,9 @@ confman confman
 #Setup awesome
 mkdir -p .config/awesome/themes
 if [ ! -d .config/awesome/themes/transBlack ]; then
-	gcl czipperz/awesomeThemes
-	cp -R awesomeThemes/trans* .config/awesome/themes
-	rm -R awesomeThemes
+    gcl czipperz/awesomeThemes
+    cp -R awesomeThemes/trans* .config/awesome/themes
+    rm -R awesomeThemes
 fi
 
 (cd b; ./install)
