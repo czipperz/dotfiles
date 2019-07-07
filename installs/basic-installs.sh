ee4bb21afde169e973bef9f4ec70bc5a75bcb289
Chris Gregory <czipperz@gmail.com>
2019-07-07 09:32:58 -0700

    Update installers

--- installs/basic-installs.sh
+++ installs/basic-installs.sh
@@ -1,17 +1,23 @@
 #!/usr/bin/bash
-yaourt -Syu
-yaourt -S zsh xorg-server xorg-server-utils xorg-apps xorg-xinit ctags
-yaourt -S awesome screenfetch pgem-git rakudo alsa-utils
-yaourt -S jdk otf-meslo-powerline-git ttf-meslo gvim scrot
-yaourt -S intellij-idea-ce-eap chromium terminator
-yaourt -S xfluxd-args emacs
+
+sudo pacman -Syu
+sudo pacman -S zsh
+sudo pacman -S xorg-server xorg-server-utils xorg-apps xorg-xinit
+sudo pacman -S i3
+sudo pacman -S otf-meslo-powerline-git ttf-meslo
+sudo pacman -S firefox
+sudo pacman -S rxvt-unicode tmux xsel
+sudo pacman -S emacs
+
 #read -p "Edit xfluxd-args settings? " -n 1 -r
 #[[ $REPLY =~ ^[Yy]$ ]] && nano /etc/xfluxd-args.conf
 #systemctl --user enable xfluxd-args
+
 echo "Installing my git applications"
-	git clone https://github.com/czipperz/g
-	(cd g; ./install)
-	rm -R g
+git clone https://github.com/czipperz/g
+(cd g; ./install)
+rm -R g
+
 gcl robbyrussell/oh-my-zsh.git .oh-my-zsh
 echo "Setting zsh as default shell"
 chsh -s $(which zsh)
--- installs/vim.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/bash
-mkdir -p $HOME/.vim/{tmp,undo,backup}
-curl https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh | sh
-cd .vim/bundle
-gcl altercation/vim-colors-solarized
-cp -R .vim/bundle/vim-colors-solarized/colors $HOME/.vim
